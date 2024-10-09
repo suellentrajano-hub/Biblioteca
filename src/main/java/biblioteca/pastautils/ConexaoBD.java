b7285fba403843d3846caad04f624fbf295ca832
suellentrajano-hub <suellen.mpt@gmail.com>
2024-10-08 21:13:23 -0400

    conexo

---
+++ src/main/java/biblioteca/pastautils/ConexaoBD.java
@@ -0,0 +1,17 @@
+package main.java.biblioteca.pastautils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+
+    public static Connection getConnection() throws SQL|Exception{
+        return DriverManager.getConnection(URL, USER, PASSWORD);    }
+    
+}
